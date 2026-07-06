a66ab819f233c758b971203814acdd0ab861a7fd
PaulSff <107915078+PaulSff@users.noreply.github.com>
2026-07-06 03:58:28 -0700

    feat: add the import_latest_workflow_graph_async into the chat utils init

--- gui/chat/utils/__init__.py
+++ gui/chat/utils/__init__.py
@@ -3,6 +3,7 @@
 from .ids import _new_id
 from .time import _now_ts
 from .ui_utils import safe_page_update, safe_update
+from .workflow_manager import import_latest_workflow_graph_async
 from .workflow_run_utils import collect_workflow_errors
 
 __all__ = [
@@ -11,4 +12,5 @@ __all__ = [
     "safe_update",
     "_now_ts",
     "_new_id",
+    "import_latest_workflow_graph_async",
 ]
