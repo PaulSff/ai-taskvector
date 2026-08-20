725a8a3b3200186cf7b367c48f78418a640df060
PaulSff <107915078+PaulSff@users.noreply.github.com>
2026-08-20 10:08:44 -0700

    refactor: generic context_gateway

---
+++ agents/chat/context_gateway/__init__.py
@@ -0,0 +1,6 @@
+"""External context gateway: teslegram chats, todos, internal/external sources, etc."""
+from .run_agentic_loop_with_context import run_agentic_turn
+
+__all__ = [
+    "run_agentic_turn",
+]
