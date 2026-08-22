804f6901e2ee3c5d63efe8c4b110075f19829e36
PaulSff <107915078+PaulSff@users.noreply.github.com>
2026-08-21 17:00:13 -0700

    refactor: remove agentic_loop unit

--- units/taskvector/agentic_loop/__init__.py
+++
@@ -1,7 +0,0 @@
-from .agentic_loop import (
-    AGENTIC_LOOP_INPUT_PORTS,
-    AGENTIC_LOOP_OUTPUT_PORTS,
-    register_agentic_loop,
-)
-
-__all__ = ["AGENTIC_LOOP_INPUT_PORTS", "AGENTIC_LOOP_OUTPUT_PORTS", "register_agentic_loop"]
