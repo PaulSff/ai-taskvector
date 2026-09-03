6f65d8cf7620e7b866aacbddba5361468a66e11b
PaulSff <107915078+PaulSff@users.noreply.github.com>
2026-09-03 06:25:17 -0700

    feat: add type ToolList for tool ids

--- agents/tools/types.py
+++ agents/tools/types.py
@@ -21,6 +21,7 @@ FOLLOW_UP_EXTRA_CLONE_ROLE_FOLLOW_UP = "clone_role_follow_up"
 FOLLOW_UP_EXTRA_LIST_DIR_FOLLOW_UP = "list_dir_follow_up"
 FOLLOW_UP_EXTRA_READ_FILE_FOLLOW_UP = "read_file_follow_up"
 
+type ToolList = tuple[str, ...]
 
 @dataclass
 class FollowUpContribution:
